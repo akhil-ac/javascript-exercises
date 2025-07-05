2f683fd94eaa0aa4b944e5e201f0ab2c067f5bba
Akhil <23039256+akhil-ac@users.noreply.github.com>
2025-07-05 22:54:57 +0530

    pseudo code first split the string and there is. .reverse() then there is join

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  let letters = string.split("");
+  letters.reverse();
+  let result = letters.join("");
+  return result;
+  console.log(result);
 };
 
 // Do not edit below this line
