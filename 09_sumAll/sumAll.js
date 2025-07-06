38ece7cf014aa79d613792297b1fc12c2273a575
Akhil <23039256+akhil-ac@users.noreply.github.com>
2025-07-06 20:24:11 +0530

    Math.min Math.max helped in starting loop

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+  let sum = 0;
+  let start = Math.min(a,b);
+  let end = Math.max(a,b);
+  if (Number.isInteger(a) && Number.isInteger(b) && a > 0 && b > 0) {
+  for (let i = start; i <= end; i++) {
+    sum += i;
+  }
+  return sum;
+} else {
+    return "ERROR";
+}
 };
 
 // Do not edit below this line
