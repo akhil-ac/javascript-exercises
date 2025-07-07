6e365b621bdbc048ffd2e855b211897f4f97caf3
Akhil <23039256+akhil-ac@users.noreply.github.com>
2025-07-07 10:06:27 +0530

    a trick Math.round(value*10)/10

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(F) {
+  let value = (F - 32) * 5 / 9;
+  return Math.round(value * 10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(C) {
+  let value = (C * 9 / 5) + 32;
+  return Math.round(value * 10)/10;
 };
 
 // Do not edit below this line
