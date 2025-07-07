1820b5b8e99420b7497b10b5714631c1e08a3e58
Akhil <23039256+akhil-ac@users.noreply.github.com>
2025-07-07 09:34:15 +0530

    if else and a condition did the job

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function(theYear) {
+  if ((theYear % 4 === 0 && theYear % 100 !== 0) || theYear % 400 === 0) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
